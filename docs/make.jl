0a446d5cb0a2f1018fef8db6dd20e88b326fc9af
Philippe Roy <borghor@yahoo.ca>
2016-12-23 15:20:03 -0500

    deploydocs function

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,10 @@
 using Documenter, ClimateTools
 
 makedocs()
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/Balinus/ClimateTools.jl.git",
+    julia  = "0.5"
+)
+
