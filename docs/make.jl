23ad9cc1f53add3e56499de156c39ea67e0e6b0a
Philippe Roy <borghor@yahoo.ca>
2016-12-23 12:02:12 -0500

    updated make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,24 @@
-push!(LOAD_PATH,"../src/")
+#push!(LOAD_PATH,"../src/")
 
 using Documenter, ClimateTools
 
-makedocs(modules = ClimateTools)
+#makedocs(modules=ClimateTools)
+
+makedocs(
+    sitename = "ClimateTools.jl",
+    modules = [ClimateTools],
+    format = :html,
+    clean = false,
+    pages = Any["Home" => "index.md"],
+)
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    target = "build",
     repo = "github.com/Balinus/ClimateTools.jl.git",
     julia  = "0.5",
     osname = "linux"
 )
-
-#makedocs(
-#    sitename = "ClimateTools.jl",
-#    modules = [ClimateTools],
-#    format = :html,
-#    clean = false,
-#    pages = Any["Home" => "index.md"],
-#)
 
 #deploydocs(
 #    repo   = "github.com/Balinus/ClimateTools.jl.git",
