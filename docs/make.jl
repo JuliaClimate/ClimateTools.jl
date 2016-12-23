6e361377d214b4ef6fb563d29b4b28daf01d4711
Philippe Roy <borghor@yahoo.ca>
2016-12-23 14:52:23 -0500

    deleted docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,35 +1,10 @@
-#push!(LOAD_PATH,"../src/")
-
 using Documenter, ClimateTools
 
-#makedocs(modules=ClimateTools)
-
 makedocs(
-    sitename = "ClimateTools.jl",
-    modules = [ClimateTools],
-    format = :html,
-    clean = false,
-    pages = Any["Home" => "index.md"],
+    doctest = false
 )
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    target = "build",
     repo = "github.com/Balinus/ClimateTools.jl.git",
-    julia  = "0.5",
-    osname = "linux"
+    julia = "0.5"
 )
-
-#deploydocs(
-#    repo   = "github.com/Balinus/ClimateTools.jl.git",
-#    target = "build",
-#    deps   = nothing,
-#    make   = nothing
-#)
-
-#deploydocs(
-#    target = "build",
-#    deps = nothing,
-#    make = nothing,
-#    repo = "github.com/Balinus/ClimateTools.jl.git",
-#)
