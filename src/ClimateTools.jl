3fa8972d4c36d469a6c6533767aab11a9c6e3e16
Philippe Roy <borghor@yahoo.ca>
2016-12-29 09:28:46 -0500

    added GMT dep

--- src/ClimateTools.jl
+++ src/ClimateTools.jl
@@ -4,6 +4,7 @@ module ClimateTools
 
 # External modules
 using NetCDF
+using GMT
 
 # Exported functions
 export windnr, leftorright, inpoly, meshgrid, boxcar3, prcp1, frostdays, summerdays, icingdays, tropicalnights, customthresover, customthresunder, annualmax, annualmin
