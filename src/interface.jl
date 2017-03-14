32fa97c714b2182df84076bbf1f6506673b0bf89
Philippe Roy <borghor@yahoo.ca>
2017-03-14 14:28:38 -0400

    Interface fix

--- src/interface.jl
+++ src/interface.jl
@@ -32,22 +32,37 @@ function getindex(C::ClimGrid,i::Int)
 end
 
 Base.linearindexing{T<:ClimGrid}(::Type{T}) = Base.LinearFast()
+Base.length(C::ClimGrid) = length(fieldnames(C))
 Base.size(::ClimGrid) = (10,)
-Base.length(C::ClimGrid) = prod(size(C))
+Base.size(C::ClimGrid,n::Int) = n==1 ? length(C) : error("Only dimension 1 has a well-defined size.")
 Base.endof(C::ClimGrid) = length(C)
+Base.ndims(::ClimGrid) = 1
 
-function Base.show(C::ClimGrid)
-  out = Dict()
-  out["Data"] = string("Data array of size ", size(C[1].data))
-  out["Model"] = C[3]
-  out["Experiment"] = C[4]
-  out["Run"] = C[5]
-  out["Data Units"] = C[2]
-  out["Latitude Units"] = C[7]
-  out["Longitude Units"] = C[6]
-  out["filename"] = C[8]
-  out["var"] = C[9]
-  out["typeof raw variable"] = C[10]
+# function summaryio(io::IO, C::ClimGrid)
+#   print(io, "ClimGrid array")
+#     # _summary(io, C)
+#     show(C)
+#     # for (name, val) in zip(axisnames(A), axisvalues(A))
+#     #     print(io, "    :$name, ")
+#     #     show(IOContext(io, :limit=>true), val)
+#     #     println(io)
+#     # end
+#     print(io, summary(C.data))
+# end
+# _summary(io, C::ClimGrid) = println(io, "$N-dimensional AxisArray{$T,$N,...} with axes:")
 
-  return out
-end
+# function Base.show(C::ClimGrid)
+#   out = Dict()
+#   out["Data"] = string("Data array of size ", size(C[1].data))
+#   out["Model"] = C[3]
+#   out["Experiment"] = C[4]
+#   out["Run"] = C[5]
+#   out["Data Units"] = C[2]
+#   out["Latitude Units"] = C[7]
+#   out["Longitude Units"] = C[6]
+#   out["filename"] = C[8]
+#   out["var"] = C[9]
+#   out["typeof raw variable"] = C[10]
+#
+#   return out
+# end
