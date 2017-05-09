4f04f48e93e88f1da40ea200d2826b14e0c9188e
Philippe Roy <borghor@yahoo.ca>
2017-05-09 12:06:15 -0400

    docs overhaul

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,16 @@
 using Documenter, ClimateTools
 
-makedocs()
+makedocs(sitename = "ClimateTools.jl",
+    pages = [
+        "index.md",
+        "gettingstarted.md"
+        "indices.md",
+        "examples.md",
+    ]
+)
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/Balinus/ClimateTools.jl.git",
     julia  = "release"
 )
-
---
+++ docs/src/indices.md
@@ -0,0 +1,11 @@
+# Indices in ClimateTools package
+
+## Annual indices
+
+```@docs
+annualmax
+```
+
+## Seasonal indices
+
+Coming soon.
