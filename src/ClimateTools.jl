4691ed46035179054e6fcdb8649fd4f6bd216c10
Philippe Roy <borghor@yahoo.ca>
2017-10-13 10:54:21 -0400

    Added specifically pyplot package

--- src/ClimateTools.jl
+++ src/ClimateTools.jl
@@ -20,10 +20,12 @@ using PyPlot
 const basemap = PyNULL()
 const np = PyNULL()
 const mpl = PyNULL()
+const plt = PyNULL()
 #const folium = PyNULL()
 
 function __init__()
   copy!(mpl, pyimport_conda("matplotlib", "matplotlib"))
+  copy!(plt, pyimport_conda("matplotlib.pyplot", "pyplot"))
   copy!(basemap, pyimport_conda("mpl_toolkits.basemap", "basemap"))
   copy!(np, pyimport_conda("numpy", "numpy"))
  # copy!(folium, pyimport_conda("folium", "folium", "conda-forge"))
