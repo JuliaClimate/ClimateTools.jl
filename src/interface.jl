b4b3f25e0a2e35623f7e902fc490cf92ee61d49e
Philippe Roy <borghor@yahoo.ca>
2018-03-19 15:05:55 -0400

    added warning

--- src/interface.jl
+++ src/interface.jl
@@ -5,10 +5,11 @@ Combines two ClimGrid. Based on the AxisArrays method. Better way to do it would
 """
 
 function Base.vcat(A::ClimGrid, B::ClimGrid)
-  axisArraytmp = vcat(A.data, B.data)
-  # TODO add axis information in the creation of the AxisArray
-  axisArray = AxisArray(axisArraytmp)
-  ClimGrid(axisArray, model = A.model, experiment = A.experiment, run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
+    warn("Use merge function instead of vcat for dimensions consistency")
+    axisArraytmp = vcat(A.data, B.data)
+    # TODO add axis information in the creation of the AxisArray
+    axisArray = AxisArray(axisArraytmp)
+    ClimGrid(axisArray, model = A.model, experiment = A.experiment, run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
 end
 
 """
@@ -18,32 +19,32 @@ Combines two ClimGrid. Based on the AxisArrays method.
 """
 
 function Base.merge(A::ClimGrid, B::ClimGrid)
-  axisArray = merge(A.data, B.data)
-  ClimGrid(axisArray, model = A.model, experiment = A.experiment, run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
+    axisArray = merge(A.data, B.data)
+    ClimGrid(axisArray, model = A.model, experiment = A.experiment, run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
 end
 
 function Base.:+(A::ClimGrid, B::ClimGrid)
-  axisArraytmp = A.data + B.data
+    axisArraytmp = A.data + B.data
 
-  axisArray = AxisArray(axisArraytmp, Axis{:time}(A[1][Axis{:time}][:]), Axis{:lon}(A[1][Axis{:lon}][:]), Axis{:lat}(A[1][Axis{:lat}][:]))
+    axisArray = AxisArray(axisArraytmp, Axis{:time}(A[1][Axis{:time}][:]), Axis{:lon}(A[1][Axis{:lon}][:]), Axis{:lat}(A[1][Axis{:lat}][:]))
 
-  ClimGrid(axisArray, model = A.model, experiment = A.experiment, run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
+    ClimGrid(axisArray, model = A.model, experiment = A.experiment, run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
 end
 
 function Base.:-(A::ClimGrid, B::ClimGrid)
-  axisArraytmp = A.data - B.data
+    axisArraytmp = A.data - B.data
 
-  axisArray = AxisArray(axisArraytmp, Axis{:time}(A[1][Axis{:time}][:]), Axis{:lon}(A[1][Axis{:lon}][:]), Axis{:lat}(A[1][Axis{:lat}][:]))
+    axisArray = AxisArray(axisArraytmp, Axis{:time}(A[1][Axis{:time}][:]), Axis{:lon}(A[1][Axis{:lon}][:]), Axis{:lat}(A[1][Axis{:lat}][:]))
 
-  ClimGrid(axisArray, model = A.model, experiment = string(A.experiment, " minus ", B.experiment), run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
+    ClimGrid(axisArray, model = A.model, experiment = string(A.experiment, " minus ", B.experiment), run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
 end
 
 function Base.:*(A::ClimGrid, k)
-  axisArraytmp = A.data * k
+    axisArraytmp = A.data * k
 
-  axisArray = AxisArray(axisArraytmp, Axis{:time}(A[1][Axis{:time}][:]), Axis{:lon}(A[1][Axis{:lon}][:]), Axis{:lat}(A[1][Axis{:lat}][:]))
+    axisArray = AxisArray(axisArraytmp, Axis{:time}(A[1][Axis{:time}][:]), Axis{:lon}(A[1][Axis{:lon}][:]), Axis{:lat}(A[1][Axis{:lat}][:]))
 
-  ClimGrid(axisArray, model = A.model, experiment = string(A.experiment, " multiplied by ", k), run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
+    ClimGrid(axisArray, model = A.model, experiment = string(A.experiment, " multiplied by ", k), run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
 end
 
 # function Base.:*(A::ClimGrid, B::ClimGrid)
@@ -55,39 +56,39 @@ end
 # end
 
 function Base.:/(A::ClimGrid, k)
-  axisArraytmp = A.data / k
+    axisArraytmp = A.data / k
 
-  axisArray = AxisArray(axisArraytmp, Axis{:time}(A[1][Axis{:time}][:]), Axis{:lon}(A[1][Axis{:lon}][:]), Axis{:lat}(A[1][Axis{:lat}][:]))
+    axisArray = AxisArray(axisArraytmp, Axis{:time}(A[1][Axis{:time}][:]), Axis{:lon}(A[1][Axis{:lon}][:]), Axis{:lat}(A[1][Axis{:lat}][:]))
 
-  ClimGrid(axisArray, model = A.model, experiment = string(A.experiment, " divided by ", k), run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
+    ClimGrid(axisArray, model = A.model, experiment = string(A.experiment, " divided by ", k), run = A.run, filename = A.filename, dataunits = A.dataunits, latunits = A.latunits, lonunits = A.lonunits, variable = A.variable, typeofvar = A.typeofvar, typeofcal = A.typeofcal)
 end
 
 function getindex(C::ClimGrid,i::Int)
-  if i == 1
-    return C.data
-  elseif i == 2
-    return C.dataunits
-  elseif i == 3
-    return C.model
-  elseif i == 4
-    return C.experiment
-  elseif i == 5
-    return C.run
-  elseif i == 6
-    return C.lonunits
-  elseif i == 7
-    return C.latunits
-  elseif i == 8
-    return C.filename
-  elseif i == 9
-    return C.variable
-  elseif i == 10
-    return C.typeofvar
-  elseif i == 11
-    return C.typeofcal
-  else
-    throw(error("You can't index like that!"))
-  end
+    if i == 1
+        return C.data
+    elseif i == 2
+        return C.dataunits
+    elseif i == 3
+        return C.model
+    elseif i == 4
+        return C.experiment
+    elseif i == 5
+        return C.run
+    elseif i == 6
+        return C.lonunits
+    elseif i == 7
+        return C.latunits
+    elseif i == 8
+        return C.filename
+    elseif i == 9
+        return C.variable
+    elseif i == 10
+        return C.typeofvar
+    elseif i == 11
+        return C.typeofcal
+    else
+        throw(error("You can't index like that!"))
+    end
 end
 
 Base.IndexStyle{T<:ClimGrid}(::Type{T}) = Base.IndexLinear()
