f6fb2fa03cb6c2055329a3ebbd28196592445a54
Philippe Roy <borghor@yahoo.ca>
2018-05-03 15:49:29 -0400

    added documenter debug key

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,12 @@ using Documenter, ClimateTools
 
 makedocs()
 
-# deploydocs(
-#     deps   = Deps.pip("mkdocs", "python-markdown-math"),
-#     repo   = "github.com/Balinus/ClimateTools.jl.git",
-#     julia  = "0.6"
-# )
+deploydocs(
+#    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/Balinus/ClimateTools.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+    julia  = "0.6",
+#    osname = "linux",
+)
