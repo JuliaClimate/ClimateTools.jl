b59556176e86b6150267a246cee4cb18feba806b
Philippe Roy <borghor@yahoo.ca>
2018-05-03 16:00:20 -0400

    changed deps

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,10 @@ using Documenter, ClimateTools
 makedocs()
 
 deploydocs(
-#    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+   deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/Balinus/ClimateTools.jl.git",
     target = "build",
-    deps = nothing,
+    # deps = nothing,
     make = nothing,
     julia  = "0.6",
 #    osname = "linux",
