2f672c6dd7215717acdb35016d9b20e8c5d9480b
Philippe Roy <borghor@yahoo.ca>
2018-05-04 12:58:49 -0400

    fixed wrong pip deps

--- docs/make.jl
+++ docs/make.jl
@@ -2,13 +2,13 @@ using Documenter, ClimateTools
 
 makedocs(sitename = "ClimateTools.jl",
     # format = :html,
-    
+
 )
 
 deploydocs(
-    # deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/Balinus/ClimateTools.jl.git",
     julia  = "0.6",
     osname = "linux",
-    deps = nothing,
+    # deps = nothing,
 )
