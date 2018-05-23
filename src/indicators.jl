33993b453cf1349ae4f3da9dcf859452af98cb14
Philippe Roy <borghor@yahoo.ca>
2018-05-23 09:27:33 -0400

    fixed history bug

--- src/indicators.jl
+++ src/indicators.jl
@@ -18,7 +18,7 @@ function vaporpressure(specific_humidity::ClimGrid, surface_pressure::ClimGrid)
   vp_dict = surface_pressure.varattribs
   vp_dict["standard_name"] = "water_vapor_pressure"
   vp_dict["units"] = "Pa"
-  vp_dict["units"] = "Water vapor pressure calculated by a function of surface_pressure and specific humidity"
+  vp_dict["history"] = "Water vapor pressure calculated by a function of surface_pressure and specific humidity"
 
   # Build ClimGrid object
   return ClimGrid(vp_array, longrid=surface_pressure.longrid, latgrid=surface_pressure.latgrid, msk=surface_pressure.msk, grid_mapping=surface_pressure.grid_mapping, dimension_dict=surface_pressure.dimension_dict, model=surface_pressure.model, frequency=surface_pressure.frequency, experiment=surface_pressure.experiment, run=surface_pressure.run, project=surface_pressure.project, institute=surface_pressure.institute, filename=surface_pressure.filename, dataunits="Pa", latunits=surface_pressure.latunits, lonunits=surface_pressure.lonunits, variable="vp", typeofvar="vp", typeofcal=surface_pressure.typeofcal, varattribs=vp_dict, globalattribs=surface_pressure.globalattribs)
@@ -73,7 +73,7 @@ function approx_surfacepressure(sealevel_pressure::ClimGrid, orography::ClimGrid
   ps_dict = sealevel_pressure.varattribs
   ps_dict["standard_name"] = "surface_pressure"
   ps_dict["units"] = "Pa"
-  ps_dict["units"] = "Surface pressure estimated with the sealevel pressure, the orography and the daily temperature"
+  ps_dict["history"] = "Surface pressure estimated with the sealevel pressure, the orography and the daily temperature"
 
   # Build ClimGrid object
   return ClimGrid(ps_array, longrid=sealevel_pressure.longrid, latgrid=sealevel_pressure.latgrid, msk=sealevel_pressure.msk, grid_mapping=sealevel_pressure.grid_mapping, dimension_dict=sealevel_pressure.dimension_dict, model=sealevel_pressure.model, frequency=sealevel_pressure.frequency, experiment=sealevel_pressure.experiment, run=sealevel_pressure.run, project=sealevel_pressure.project, institute=sealevel_pressure.institute, filename=sealevel_pressure.filename, dataunits="Pa", latunits=sealevel_pressure.latunits, lonunits=sealevel_pressure.lonunits, variable="ps", typeofvar="ps", typeofcal=sealevel_pressure.typeofcal, varattribs=ps_dict, globalattribs=sealevel_pressure.globalattribs)
