c6e2297394f49ecf705a1eef71f0a1b794908c16
DominiqueCaron <dominique.caron.1@umontreal.ca>
2018-06-14 17:28:20 -0400

    add test_detrend not working

--- test/biascorrect_test.jl
+++ test/biascorrect_test.jl
@@ -28,9 +28,41 @@ axisdata = AxisArray(data, Axis{:lon}(1:6), Axis{:lat}(1:6), Axis{:time}(d))
 obs = ClimGrid(axisdata, variable = "tasmax", dimension_dict=dimension_dict)
 ref = obs - 3
 
-ITP = qqmaptf(obs, ref, partition=0.5)
+ITP = qqmaptf(obs, ref, partition=0.5, detrend = false)
 @test round(ITP.itp[rand(1:365)][randn(1)][1],10) == 3.0
 
 fut = obs - 3
 D = qqmap(fut, ITP)
 @test D[1][1, 1, 1] == -0.5560268761463862
+
+#
+dimension_dict = Dict(["lon" => "lon", "lat" => "lat"])
+d = Date(1961,1,1):Date(1990,12,31)
+srand(42)
+data = randn(6, 6, 10957)
+axisdata = AxisArray(data, Axis{:lon}(1:6), Axis{:lat}(1:6), Axis{:time}(d))
+obs = ClimGrid(axisdata, variable = "tasmax", dimension_dict=dimension_dict)
+ref = obs - 3
+
+ITP = qqmaptf(obs, ref, partition=0.5, detrend = true)
+@test round(ITP.itp[rand(1:365)][randn(1)][1],10) == 3.0
+
+fut = obs - 3
+D = qqmap(fut, ITP)
+@test D[1][1, 1, 1] == -0.5560268761463862
+
+
+# Create a ClimGrid with a clear trend
+x = 1:10957
+x = x
+d = Date(1961,1,1):Date(1990,12,31)
+data = zeros(2, 2, 10957)
+data[1,1,:] = 1 + x
+data[1,2,:] = 0.5 + 0.2.*x + x.^2
+data[2,1,:] = 0.25 + 0.04.*x - 0.01 .* x.^3
+data[2,2,:] = 10 + 0.14.*x.^2 - 0.032 .* x .^4
+axisdata = AxisArray(data, Axis{:lon}(1:2), Axis{:lat}(1:2), Axis{:time}(d))
+C = ClimGrid(axisdata)
+poly = ClimGridpolyfit(C)
+val = ClimGridpolyval(C,poly)
+D = C - val
