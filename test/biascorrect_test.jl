ca9d524ba14c5fb3cd1a88c0c0ee5aec069bb4cc
Philippe Roy <borghor@yahoo.ca>
2018-06-19 13:37:57 -0400

    added test for detrend=true

--- test/biascorrect_test.jl
+++ test/biascorrect_test.jl
@@ -9,6 +9,8 @@ fut = obs * 1.05
 
 D = qqmap(obs, ref, fut, method = "Additive", detrend=false)
 @test D[1][1, 1, 1] == -0.5560268761463861
+D = qqmap(obs, ref, fut, method = "Additive", detrend=true)
+@test D[1][1, 1, 1] == -0.5560274620422795
 
 srand(42)
 data = randn(2, 2, 10957)
