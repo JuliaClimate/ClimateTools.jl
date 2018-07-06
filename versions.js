bdd72ead532420df43bb1e4f73c53129930a0229
autodocs <autodocs>
2018-07-06 01:01:52 +0000

    build based on d59380e

--- versions.js
+++ versions.js
@@ -1,10 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.8",
   "release-0.7",
   "release-0.6",
   "release-0.1",
   "release-0.0",
+  "v0.8.0",
   "v0.7.0",
   "v0.6.0",
   "v0.1.2",
