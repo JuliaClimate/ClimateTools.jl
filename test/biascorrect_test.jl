d10c9bc9bacc4ce873db63162696b5b55b7d56ef
Philippe Roy <borghor@yahoo.ca>
2018-07-12 10:43:32 -0400

    rounded tests

--- test/biascorrect_test.jl
+++ test/biascorrect_test.jl
@@ -13,7 +13,7 @@ fut = obs * 1.05
 D = qqmap(obs, ref, fut, method = "Additive", detrend=false)
 @test D[1][1, 1, 1] == -0.5560268761463861
 D = qqmap(obs, ref, fut, method = "Additive", detrend=true)
-@test D[1][1, 1, 1] == -0.5560268760568654
+@test round(D[1][1, 1, 1], 5) == -0.55603#74620422795
 
 srand(42)
 data = randn(2, 2, 10957)
@@ -65,7 +65,7 @@ ITP = qqmaptf(obs, ref, partition=0.5, detrend = true, method="multiplicative")
 @test round(ITP.itp[rand(1:365)][randn(1)][1],1) == 0.0
 fut = obs - 3
 D = qqmap(fut, ITP)
-@test D[1][1, 1, 1] == -0.5559875611239462
+@test round(D[1][1, 1, 1], 5) == -0.55599#75611239462
 
 
 # Create a ClimGrid with a clear trend
