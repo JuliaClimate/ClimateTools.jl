8333ae47f556ce5e6c3aabfca89cf91724634880
Philippe Roy <borghor@yahoo.ca>
2018-11-20 21:16:10 -0500

    basemap tarball and python2

--- deps/build.jl
+++ deps/build.jl
@@ -1,16 +1,16 @@
 using Pkg
 
 if lowercase(get(ENV, "CI", "false")) == "true"
-    let basepython = get(ENV, "PYTHON", "python")
+    let basepython = get(ENV, "PYTHON", "python2")
         envpath = joinpath(@__DIR__, "env")
         run(`virtualenv --python=$basepython $envpath`)
 
         if Sys.iswindows()
             python = joinpath(envpath, "Scripts", "python.exe")
         else
-            python = joinpath(envpath, "bin", "python")
+            python = joinpath(envpath, "bin", "python2")
         end
-        run(`$python -m pip install basemap`)
+        run(`$python -m pip install https://github.com/matplotlib/basemap/archive/v1.0.7rel.tar.gz`)
         run(`$python -m pip install scipy`)
         run(`$python -m pip install matplotlib`)
         run(`$python -m pip install cmocean`)
