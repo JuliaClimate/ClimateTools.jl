e8f0bc2f1ad5db6a062936708346fc56d60e2ef6
Philippe Roy <borghor@yahoo.ca>
2018-11-24 11:59:02 -0500

    fixed character literal

--- deps/build.jl
+++ deps/build.jl
@@ -7,7 +7,7 @@ if lowercase(get(ENV, "CI", "false")) == "true"
 
     let basepython = get(ENV, "PYTHON", "python2")
         envpath = joinpath(@__DIR__, "env")
-        run('pip install --user virtualenv')
+        run(`pip install --user virtualenv`)
         run(`virtualenv --python=$basepython $envpath`)
 
         if Sys.iswindows()
