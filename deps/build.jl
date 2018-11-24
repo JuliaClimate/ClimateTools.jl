9bedf099c175eda8ab8a903d0c8bac35e900f18a
Philippe Roy <borghor@yahoo.ca>
2018-11-24 15:39:45 -0500

    reverted --user install

--- deps/build.jl
+++ deps/build.jl
@@ -7,7 +7,7 @@ if lowercase(get(ENV, "CI", "false")) == "true"
 
     let basepython = get(ENV, "PYTHON", "python2")
         envpath = joinpath(@__DIR__, "env")
-        run(`pip install --user virtualenv`)
+        run(`pip install virtualenv`)
         run(`virtualenv --python=$basepython $envpath`)
 
         if Sys.iswindows()
@@ -21,12 +21,12 @@ if lowercase(get(ENV, "CI", "false")) == "true"
             # end
         end
         # run(`sudo apt-get install libnetcdf-dev`)
-        run(`$python -m pip install --user numpy`)
-        run(`$python -m pip install --user scipy`)
-        run(`$python -m pip install --user matplotlib`)
-        run(`$python -m pip install --user https://github.com/matplotlib/basemap/archive/v1.0.7rel.tar.gz`)
-        run(`$python -m pip install --user git+https://github.com/matplotlib/cmocean`)
-        run(`$python -m pip install --user netcdf4`)
+        run(`$python -m pip install numpy`)
+        run(`$python -m pip install scipy`)
+        run(`$python -m pip install matplotlib`)
+        run(`$python -m pip install https://github.com/matplotlib/basemap/archive/v1.0.7rel.tar.gz`)
+        run(`$python -m pip install git+https://github.com/matplotlib/cmocean`)
+        run(`$python -m pip install netcdf4`)
 
         ENV["PYTHON"] = python
         Pkg.build("PyCall")
