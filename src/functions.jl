527a790649241dea2ee4bf013a40e1561cfd039b
Philippe Roy <borghor@yahoo.ca>
2018-12-04 14:27:41 -0500

    re-added extension function

--- src/functions.jl
+++ src/functions.jl
@@ -595,6 +595,41 @@ function get_units(A)
 
 end
 
+"""
+    periodmean(C::ClimGrid, startdate::Tuple, enddate::Tuple)
+
+Mean of array data over a given period.
+"""
+function periodmean(C::ClimGrid, startdate::Tuple, enddate::Tuple)
+    Csubset = temporalsubset(C, startdate, enddate)
+    datain   = Csubset.data.data
+
+    # Mean and squeeze
+    dataout = Statistics.mean(datain, dims=3)
+
+    # Build output AxisArray
+    FD = buildarray_climato(C, dataout)
+
+    # Return climGrid type containing the indice
+    return ClimGrid(FD, longrid=C.longrid, latgrid=C.latgrid, msk=C.msk, grid_mapping=C.grid_mapping, dimension_dict=C.dimension_dict, model=C.model, frequency=C.frequency, experiment=C.experiment, run=C.run, project=C.project, institute=C.institute, filename=C.filename, dataunits=C.dataunits, latunits=C.latunits, lonunits=C.lonunits, variable="periodmean", typeofvar=C.typeofvar, typeofcal="climatology", varattribs=C.varattribs, globalattribs=C.globalattribs)
+end
+
+function extension(url::String)
+    try
+        return match(r"\.[A-Za-z0-9]+$", url).match
+    catch
+        return ""
+    end
+end
+
+"""
+    function applyunits(C::ClimGrid)
+
+Returns the adequate units of Unitful of ClimGrid C.
+"""
+function get_units(C)
+    return unit(C[1])
+end
 
 # function rot2lonlat(lon, lat, SP_lon, SP_lat; northpole = true)
 #
