f71cf7e36008fc4ea55531d60752c775c74a7833
Philippe Roy <borghor@yahoo.ca>
2018-12-20 19:20:18 -0500

    fixed polygon test

--- test/spatial_test.jl
+++ test/spatial_test.jl
@@ -2,7 +2,7 @@
 
     fileshp = joinpath(dirname(@__FILE__), "data", "SudQC_GCM.shp")
     P = extractpoly(fileshp, 1)
-    @test size(P) = (2,6)
+    @test size(P) == (2,6)
     @test isnan(P[1,1])
     @test isnan(P[2,1])
 
