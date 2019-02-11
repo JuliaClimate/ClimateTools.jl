3c5b0e214340335a0c3a34be41376c4b1a03c1a8
Philippe Roy <borghor@yahoo.ca>
2019-02-11 10:30:58 -0500

    added test for new region and simplified calls

--- test/mapping_test.jl
+++ test/mapping_test.jl
@@ -84,22 +84,11 @@ status, figh = mapclimgrid(spatialsubset(C, P), mask = msk);@test status == true
 C = load(filenc, "ua", poly = P)
 status, figh = mapclimgrid(C);@test status == true; PyPlot.close() # feeding a 4D field
 
-# empty map generation
-status, figh = mapclimgrid(region = "World");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "WorldAz");@test status == true; PyPlot.close()
-# status, figh = mapclimgrid(region = "WorldEck4");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "Canada");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "Quebec");@test status == true; PyPlot.close()
-# status, figh = mapclimgrid(region = "QuebecNSP");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "Americas");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "NorthAmerica");@test status == true; PyPlot.close()
-# status, figh = mapclimgrid(region = "SouthAmerica");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "Greenwich");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "outaouais");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "laurentides");@test status == true; PyPlot.close()
-status, figh = mapclimgrid(region = "estrie");@test status == true; PyPlot.close()
-# status, figh = mapclimgrid(region = "Arctic");@test status == true; PyPlot.close()
-# status, figh = mapclimgrid(region = "Antarctic");@test status == true; PyPlot.close()
+regions = ["World", "WorldAz", "WorldEck4", "Canada", "Quebec", "Quebec_agricole", "South_Quebec", "QuebecNSP", "Americas", "NorthAmerica", "SouthAmerica", "Greenwich", "Outaouais", "Laurentides", "Estrie", "Arctic", "Antarctic", "Africa", "Europe"]
+
+for iregion in regions
+    status, figh = mapclimgrid(region=iregion);@test status == true; PyPlot.close()
+end
 
 # DUMMY MAPS
 lon = collect(-180.0:180.0)
