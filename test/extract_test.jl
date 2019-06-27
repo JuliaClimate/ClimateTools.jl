2f599b644928c8164b50045984e8ae27c1c85466
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2019-06-27 14:52:57 -0400

    add test for polar_stereographic grid

--- test/extract_test.jl
+++ test/extract_test.jl
@@ -40,7 +40,7 @@ D = resample(C, "SON") # hardcoded seasons
 
 @test_throws ErrorException D = resample(C, "df")
 
-maps = ["rotated_pole", "rotated_mercator", "crs", "lambert_conformal_conic","rotated_latitude_longitude", "None"]
+maps = ["rotated_pole", "rotated_mercator", "crs", "lambert_conformal_conic", "rotated_latitude_longitude", "polar_stereographic", "None"]
 
 for imap in maps
   K = ["time", imap]
