172aa60e71275cfa01c7a17f15fd8b5191a67cbf
Philippe Roy <borghor@yahoo.ca>
2019-07-04 10:17:35 -0400

    developing

--- src/biascorrect.jl
+++ src/biascorrect.jl
@@ -198,11 +198,135 @@ function qqmap(obsvec::Array{N,1} where N, refvec::Array{N,1} where N, futvec::A
 end
 
 """
-    biascorrect_extremes(obs::ClimGrid, ref::ClimGrid, fut::ClimGrid, μ, σ, ξ)
+    biascorrect_extremes(obs::ClimGrid, ref::ClimGrid, fut::ClimGrid; method::String="Additive", detrend::Bool=true, window::Int=15, rankn::Int=50, thresnan::Float64=0.1, keep_original::Bool=false, interp=Linear(), extrap=Flat(), gev_param)
+
+Correct the tail of the distribution with a paramatric method, using the parameters μ, σ and ξ contained in gev_params.
+"""
+function biascorrect_extremes(obs::ClimGrid, ref::ClimGrid, fut::ClimGrid; method::String="Additive", P::Real=0.95, detrend::Bool=true, window::Int=15, rankn::Int=50, thresnan::Float64=0.1, keep_original::Bool=false, interp=Linear(), extrap=Flat(), gev_params::DataFrame)
+
+    # Consistency checks # TODO add more checks for grid definition
+    @argcheck size(obs[1], 1) == size(ref[1], 1) == size(fut[1], 1)
+    @argcheck size(obs[1], 2) == size(ref[1], 2) == size(fut[1], 2)
+
+    # Modify dates (e.g. 29th feb are dropped/lost by default)
+    obs = ClimateTools.dropfeb29(obs)
+    ref = ClimateTools.dropfeb29(ref)
+    fut = ClimateTools.dropfeb29(fut)
+
+    # Remove trend if specified
+    if detrend == true
+        # Obs
+        obs_polynomials = ClimateTools.polyfit(obs)
+        obs = obs - ClimateTools.polyval(obs, obs_polynomials)
+        # Ref
+        ref_polynomials = ClimateTools.polyfit(ref)
+        ref = ref - ClimateTools.polyval(ref, ref_polynomials)
+        # Fut
+        fut_polynomials = ClimateTools.polyfit(fut)
+        poly_values = ClimateTools.polyval(fut, fut_polynomials)
+        fut = fut - poly_values
+    end
+
+    #Get date vectors
+    datevec_obs = get_timevec(obs)
+    datevec_ref = get_timevec(ref)
+    datevec_fut = get_timevec(fut)
+
+    # Julian days vectors
+    obs_jul = Dates.dayofyear.(datevec_obs)
+    ref_jul = Dates.dayofyear.(datevec_ref)
+    fut_jul = Dates.dayofyear.(datevec_fut)
+
+    # Prepare output array (replicating data type of fut ClimGrid)
+    dataout = fill(convert(typeof(fut[1].data[1]), NaN), (size(fut[1], 1), size(fut[1],2), size(fut[1], 3)))::Array{typeof(fut[1].data[1]), T} where T
+
+    if minimum(ref_jul) == 1 && maximum(ref_jul) == 365
+        days = 1:365
+    else
+        days = minimum(ref_jul)+window:maximum(ref_jul)-window
+        start = Dates.monthday(minimum(datevec_obs))
+        finish = Dates.monthday(maximum(datevec_obs))
+    end
+
+    # Reshape. Allows multi-threading on grid points.
+    obsin = reshape(obs[1].data, (size(obs[1].data, 1)*size(obs[1].data, 2), size(obs[1].data, 3)))
+    refin = reshape(ref[1].data, (size(ref[1].data, 1)*size(ref[1].data, 2), size(ref[1].data, 3)))
+    futin = reshape(fut[1].data, (size(fut[1].data, 1)*size(fut[1].data, 2), size(fut[1].data, 3)))
+    dataoutin = reshape(dataout, (size(dataout, 1)*size(dataout, 2), size(dataout, 3)))
+
+    # Looping over grid points using multiple-dispatch calls to qqmap
+    Threads.@threads for k = 1:size(obsin, 1)
+
+        obsvec = obsin[k,:]
+        refvec = refin[k,:]
+        futvec = futin[k,:]
+
+        # Find GEV parameters (closest grid points)
+
+        dataoutin[k, :] = qqmap(obsvec, refvec, futvec, days, obs_jul, ref_jul, fut_jul, method=method, detrend=detrend, window=window, rankn=rankn, thresnan=thresnan, keep_original=keep_original, interp=interp, extrap=extrap)
+
+    end
+
+    # Apply mask
+    dataout = applymask(dataout, obs.msk)
+
+    # Rebuilding ClimGrid
+    lonsymbol = Symbol(fut.dimension_dict["lon"])
+    latsymbol = Symbol(fut.dimension_dict["lat"])
+
+    dataout2 = AxisArray(dataout, Axis{lonsymbol}(fut[1][Axis{lonsymbol}][:]), Axis{latsymbol}(fut[1][Axis{latsymbol}][:]),Axis{:time}(datevec_fut))
+
+    timeattrib = fut.timeattrib
+    timeattrib["calendar"] = "365_day"
+
+    globalattribs = fut.globalattribs
+    if haskey(globalattribs, "history")
+        globalattribs["history"] = string(globalattribs["history"], " - ", "Bias-corrected with ClimateTools.jl")
+    else
+        globalattribs["history"] = string("Bias-corrected with ClimateTools.jl")
+    end
+
+    C = ClimGrid(dataout2; longrid=fut.longrid, latgrid=fut.latgrid, msk=fut.msk, grid_mapping=fut.grid_mapping, dimension_dict=fut.dimension_dict, timeattrib=timeattrib, model=fut.model, frequency=fut.frequency, experiment=fut.experiment, run=fut.run, project=fut.project, institute=fut.institute, filename=fut.filename, dataunits=fut.dataunits, latunits=fut.latunits, lonunits=fut.lonunits, variable=fut.variable, typeofvar=fut.typeofvar, typeofcal="365_day", varattribs=fut.varattribs, globalattribs=globalattribs)
+
+    if detrend == true
+        C = C + poly_values
+    end
+
+    return C
+
+end
+
+"""
+    biascorrect_extremes(obs::AxisArray, ref::AxisArray, fut::AxisArray, μ, σ, ξ)
 
 Correct the tail of the distribution with a paramatric method, using the parameters μ, σ and ξ.
 """
-function biascorrect_extremes(obs::ClimGrid, ref::ClimGrid, fut::ClimGrid, μ::Real, σ::Real, ξ::Real)
+function biascorrect_extremes(obsvec::AxisArray, refvec::AxisArray, futvec::AxisArray, days, μ::Real, σ::Real, ξ::Real)
+
+    obs_jul = dayofyear.(obsvec[Axis{:time}][:])
+    ref_jul = dayofyear.(refvec[Axis{:time}][:])
+    fut_jul = dayofyear.(futvec[Axis{:time}][:])
+
+    # Prepare output array (replicating data type of fut ClimGrid)
+    dataout = fill(convert(typeof(futvec.data[1]), NaN), length(futvec))::Array{typeof(fut[1].data[1]), T} where T
+
+    if minimum(ref_jul) == 1 && maximum(ref_jul) == 365
+        days = 1:365
+    else
+        days = minimum(ref_jul)+window:maximum(ref_jul)-window
+        start = Dates.monthday(minimum(datevec_obs))
+        finish = Dates.monthday(maximum(datevec_obs))
+    end
+
+    threshold = mean([quantile(obsvec[obsvec .>= 1.0],P) quantile(refvec[refvec .>= 1.0], P)])
+
+    idx_obs = obsvec .> threshold
+    idx_ref = refvec .> threshold
+    idx_fut = futvec .> threshold
+
+    dataout .= qqmap(obsvec.data, refvec.data, futvec.data, days, obs_jul, ref_jul, fut_jul, method="multiplicative", detrend=false)
+
+
 
 
 end
