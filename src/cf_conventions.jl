d87365d3aefc5f01f9e4195d76086c06ad9ab0c2
Philippe Roy <borghor@yahoo.ca>
2020-03-14 13:43:16 -0400

    function description

--- src/cf_conventions.jl
+++ src/cf_conventions.jl
@@ -1,3 +1,8 @@
+"""
+    get_dimname(::NCDatasets.Dataset, dim::String)
+
+Scans dataset ds and extract the dimensions X, Y, Z and T. The dataset needs to be CF-compliant. See http://cfconventions.org/.
+"""
 function get_dimname(ds::NCDatasets.Dataset, dim::String)
 
     dimensions = keys(ds.dim)
