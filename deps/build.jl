58f667da63e945ebf40a3755b4afe28ec76db292
Philippe Roy <borghor@yahoo.ca>
2020-04-29 15:09:43 -0400

    added explicit PyCall

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,9 @@
 using Pkg
+using PyCall
 
-if lowercase(get(ENV, "CI", "false")) == "true"    
+if lowercase(get(ENV, "CI", "false")) == "true"
 
         ENV["PYTHON"] = ""
         Pkg.build("PyCall")
-      
+
 end
